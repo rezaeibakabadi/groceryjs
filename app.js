1fdb117ed68855757af976585cb605592411de6a
unknown <reza.eibak@gmail.com>
2022-06-24 18:39:48 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.querySelector('#grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+// edit option
+let editElement;
+let editFlag = false;
+let editId = "";
+
+// ****** EVENT LISTENERS **********
+form.addEventListener("submit",addItem);
+// ****** FUNCTIONS **********
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+if( value && !editFlag ){
+    console.log('add item to the list')
+}else if( value  && editFlag){
+    console.log('editing')
+}else{
+    alertDisplay('please enter value','danger')
+}
+}
+// alert display
+function alertDisplay(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+
+    // time out
+    setTimeout(function(){
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+    },3000)
+}
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
